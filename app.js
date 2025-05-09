0607043d7d73e84b08ee7c97cd4ad30cd6d9e17e
AzizullahCodes <azizullahcodes@gmail.com>
2025-05-09 12:34:49 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,65 @@
+// =================js-assignment date May-08-2025 First Assignment===================
+// =============Question no 1: Alert============
+// var vistiorName = "Hadi";
+// alert("I say welcome to " + vistiorName + " in my website!");
+
+// ==========Question no 2: Alert===============
+// alert("Error! Please enter a valid password.");
+
+// ==========Question no 3: Alert===============
+// alert("Welcome to Js Land... \n Happy Coding!");
+
+// ==========Question no 4: Alert===============
+// alert("Welcome to Js Land...");
+
+// alert("Prevent this page from coding additional dialogs.")
+
+// ==========Question no 5: Alert===============
+// console.log("Hello... I run can Js through my web browser's console");
+// ==========Question no 6 : Variable strings & Numbers===============
+// a
+// var userName = "Kaleem";
+// alert(userName);
+// b
+// var myName = "Azizullah";
+// console.log(myName);
+
+// c
+
+// var message = "Hello World";
+// alert(message);
+
+
+// ==========Question no 7: Student Bio data===============
+//  var studentName = "Hashir";
+//  var age = 20;
+//  var institute = "SMIT";
+//  var course = "Modern Website & Mobile App Development";
+//  console.log(studentName + " " +  age + " " + institute + " "+ course);
+
+// ===========Question no 8 own age in console=================
+
+// var age = 30;
+// console.log("I am " + age + " years old.");
+
+// ===========Question no 9/ Birth year============
+// var myBirthYear = 2000;
+// alert("My birth year is " + myBirthYear + "\n" +"Data type of my declared in number");
+// ===============Question no 10/ one js varibale multiple line breaking================
+// var lineBreaking = "pizz \npizz \npiz \npi \np";
+// console.log(lineBreaking);
+
+// ============Question no 11 Fortune Teller===============
+// var numberOfChildern = 3;
+// var partner_name = "uyik";
+// var geographic_Location = "UAE";
+// var job_Title = "Pilot";
+// alert("You will be " + job_Title + " in " + geographic_Location + " and married to " + partner_name + " with " + numberOfChildern + " kids.");
+
+// ==========Questin no 12/ string concatenation===========
+// var email = "abc97890@gmail.com";
+// console.log("My email address is " + email);
+// alert("My email address is " + email);
+
+// var book = "A smarter way to learn javascript";
+// alert("I am trying to learn form the book " + " ' " + book + " ' ");
